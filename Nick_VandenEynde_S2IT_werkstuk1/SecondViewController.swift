b3c5dd6b24cb98bb8deab3b8b157d95eaf8456a9
Nick VDE <ninjawulf98@gmail.com>
2018-04-27 21:06:21 +0200

    The demon strikes back

--- Nick_VandenEynde_S2IT_werkstuk1/SecondViewController.swift
+++ Nick_VandenEynde_S2IT_werkstuk1/SecondViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var myMapView: MKMapView!
     var locationManager = CLLocationManager()
     var personen = [Persoon]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestAlwaysAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
         
         personen.append(Persoon(naam: "Den Imbessielen Boer", voornaam: "Jos", foto: UIImage(named: "foto1")!, adres:  Adres(straat: "ZeemeeuwStraat", huisnummer: 45, postcode: 1765, gemeente: "Den Lemburg"), locatie: CLLocationCoordinate2DMake(50.852183, 4.343737), telefoonnummer: "05352359392395"))
         
@@ -24,8 +28,7 @@ class SecondViewController: UIViewController {
         
         personen.append(Persoon(naam: "Van den Delhaize", voornaam: "André", foto: UIImage(named: "foto3")!, adres:  Adres(straat: "ZeemeeuwStraat", huisnummer: 45, postcode: 1945, gemeente: "Merchtem City"), locatie: CLLocationCoordinate2DMake(50.812183, 4.353737), telefoonnummer: "05352359493095"))
         //       locationManager.delegate = self
-        locationManager.requestAlwaysAuthorization()
-        locationManager.startUpdatingLocation()
+        
         
         for person in personen {
             let myAnnotation = MyAnnotation(coordinate: person.locatie, title: person.voornaam)
@@ -41,10 +44,21 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+//    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+//        let center = CLLocationCoordinate2D(latitude: 50.812183, longitude: 4.353737)
+//        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+//        let region = MKCoordinateRegion(center: center, span: span)
+//        mapView.setRegion(region, animated: true)
+//    }
+//
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        
+
         mapView.setRegion(region, animated: true)
     }
 
